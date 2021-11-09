f279ad3f3250814350ab0ebb5cf22d0702e8ac33
mmantle-hud <m.e.mantle@hud.ac.uk>
2021-11-09 10:24:51 +0000

    Update browseable-list-view.php

--- views/browseable-list-view.php
+++ views/browseable-list-view.php
@@ -5,18 +5,15 @@ include "header.php";
 <h1>A Browseable List</h1>
 <?php
 
-//check to see if there are any results
-if($films){
-	//loop over the array of films and display their titles
-	foreach ($films as $film) {
-	    echo "<p>";
-	    echo "<a href='details-new.php?id={$film["id"]}'>";
-	    echo $film['title'];
-		echo "</a>";
-	    echo "</p>";
-	}
-}else{
-	echo "No records found";
+
+//loop over the array of films and display their titles
+foreach ($films as $film) {
+    echo "<p>";
+    echo "<a href='details-new.php?id={$film["id"]}'>";
+    echo $film['title'];
+    echo "</a>";
+    echo "</p>";
 }
+
 include "footer.php";
 ?>
