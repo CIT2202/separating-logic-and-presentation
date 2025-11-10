c6091c2f33f84a8319db52909c619f71e945f0bb
Matthew Mantle <m.e.mantle@hud.ac.uk>
2025-11-10 10:39:27 +0000

    Updated with require

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require "connect.php";
 //select all the films
-$query = "SELECT * FROM films";
+$query = "SELECT id, title FROM films";
 $resultset = $conn->query($query);
 $films = $resultset->fetchAll();
 $conn = NULL;
