2d96c284c2572abcd3d9eb2ab0367ef6259d5302
Matthew Mantle <m.e.mantle@hud.ac.uk>
2025-11-14 11:19:13 +0000

    Update header.php

--- views/header.php
+++ views/header.php
@@ -13,3 +13,10 @@
 <meta http-equiv="content-type" content="text/html;charset=utf-8">
 </head>
 <body>
+<nav>
+	<ul>
+		<li><a href="index.php">Home</a></li>
+		<li><a href="create.php">Add new film</a></li>
+		<li><a href="about.php">About</a></li>
+	</ul>
+</nav>	
